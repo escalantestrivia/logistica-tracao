950928b4309cb12ca02b4784f827c963b3255c98
escalantestrivia <roger.natael@triviatrens.com.br>
2026-07-18 06:45:13 -0300

    Update checklist.js

--- js/checklist.js
+++ js/checklist.js
@@ -176,3 +176,93 @@ rows="4"></textarea>
     carregarChecklist();
 
 }
+
+
+function calcularGestao(){
+
+    const operadores =
+        Number(document.getElementById("operadores").value)||0;
+
+    const ausencias =
+        Number(document.getElementById("ausencias").value)||0;
+
+    const frota =
+        Number(document.getElementById("frota").value)||0;
+
+    const viras =
+        Number(document.getElementById("viras").value)||0;
+
+    const escala =
+        Number(document.getElementById("escala").value)||0;
+
+    const locomotiva =
+        Number(document.getElementById("locomotiva").value)||0;
+
+    document.getElementById("gestao").value =
+        operadores -
+        ausencias -
+        frota -
+        viras -
+        escala -
+        locomotiva;
+
+}
+
+function salvarChecklist(){
+
+    const chave = obterChave();
+
+    if(!banco[chave]) return;
+
+    banco[chave].checklist = {
+
+        operadores:document.getElementById("operadores").value,
+
+        ausencias:document.getElementById("ausencias").value,
+
+        frota:document.getElementById("frota").value,
+
+        viras:document.getElementById("viras").value,
+
+        escala:document.getElementById("escala").value,
+
+        locomotiva:document.getElementById("locomotiva").value,
+
+        gestao:document.getElementById("gestao").value,
+
+        observacoes:document.getElementById("observacoes").value
+
+    };
+
+    salvarBanco();
+
+}
+
+function carregarChecklist(){
+
+    montarChecklist();
+
+    const chave = obterChave();
+
+    if(!banco[chave]) return;
+
+    const c = banco[chave].checklist;
+
+    document.getElementById("operadores").value = c.operadores || 0;
+
+    document.getElementById("ausencias").value = c.ausencias || 0;
+
+    document.getElementById("frota").value = c.frota || 0;
+
+    document.getElementById("viras").value = c.viras || 0;
+
+    document.getElementById("escala").value = c.escala || 0;
+
+    document.getElementById("locomotiva").value = c.locomotiva || 0;
+
+    document.getElementById("observacoes").value =
+        c.observacoes || "";
+
+    calcularGestao();
+
+}
