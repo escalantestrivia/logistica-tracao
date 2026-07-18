5a37d4dd24449beb46bf622bf9325d4197fb4b14
escalantestrivia <roger.natael@triviatrens.com.br>
2026-07-18 07:37:21 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,52 @@ const relatorio = {
     fatos: [],
     locomotivas: []
 };
+function iniciarRelatorio() {
 
+    relatorio.identificacao = {
+
+        nome: document.getElementById("txtNome").value,
+
+        matricula: document.getElementById("txtMatricula").value,
+
+        posto: document.getElementById("txtPosto").value,
+
+        local: document.getElementById("cmbLocal").value,
+
+        turno: document.getElementById("cmbTurno").value,
+
+        data: document.getElementById("txtData").value
+
+    };
+
+    document.getElementById("nomeOperador").textContent =
+        relatorio.identificacao.nome;
+
+    document.getElementById("matriculaOperador").textContent =
+        relatorio.identificacao.matricula;
+
+    document.getElementById("posto").textContent =
+        relatorio.identificacao.posto;
+
+    document.getElementById("local").textContent =
+        relatorio.identificacao.local;
+
+    document.getElementById("turno").textContent =
+        relatorio.identificacao.turno;
+
+    document.getElementById("data").textContent =
+        relatorio.identificacao.data;
+
+    document.querySelectorAll(".menu-item").forEach(botao => {
+
+        if (botao.dataset.view !== "identificacao")
+            botao.disabled = false;
+
+    });
+
+    carregarView("checklist");
+
+}
 async function carregarView(nome) {
 
     const resposta = await fetch(`views/${nome}.html`);
