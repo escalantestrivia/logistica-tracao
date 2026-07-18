d97770da38f10b36329ddb14cb560848d3a86bb5
escalantestrivia <roger.natael@triviatrens.com.br>
2026-07-18 08:18:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
+// Verifica se existe um usuário logado
 const usuario = JSON.parse(localStorage.getItem("usuario"));
 
 if (!usuario) {
-
-    location.href = "login.html";
-
+    window.location.href = "login.html";
 }
+
+// Preenche o cabeçalho
+document.getElementById("lblNome").textContent = usuario.nome;
+document.getElementById("lblMatricula").textContent = usuario.matricula;
+
+// Preenche a tela Identificação
+document.getElementById("txtNome").value = usuario.nome;
+document.getElementById("txtMatricula").value = usuario.matricula;
 
 document.getElementById("lblNome").innerHTML = usuario.nome;
 
