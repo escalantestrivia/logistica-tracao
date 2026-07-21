8680cee373df73f97c8790cf55d26538d50ffc99
escalantestrivia <roger.natael@triviatrens.com.br>
2026-07-20 21:05:56 -0300

    Add files via upload

---
+++ linha11.js
@@ -0,0 +1,44 @@
+const linha11 = [
+
+
+    { id: "S004",  tue: "8001 - 8004", cabine1: "8001", cabine2: "8004" },
+    { id: "S008",  tue: "8005 - 8008", cabine1: "8005", cabine2: "8008" },
+    { id: "S012",  tue: "8009 - 8012", cabine1: "8009", cabine2: "8012" },
+    { id: "S016",  tue: "8013 - 8016", cabine1: "8013", cabine2: "8016" },
+    { id: "S020",  tue: "8017 - 8020", cabine1: "8017", cabine2: "8020" },
+    { id: "S024",  tue: "8021 - 8024", cabine1: "8021", cabine2: "8024" },
+    { id: "S028",  tue: "8025 - 8028", cabine1: "8025", cabine2: "8028" },
+    { id: "S032",  tue: "8029 - 8032", cabine1: "8029", cabine2: "8032" },
+    { id: "S036",  tue: "8033 - 8036", cabine1: "8033", cabine2: "8036" },
+    { id: "S040",  tue: "8037 - 8040", cabine1: "8037", cabine2: "8040" },
+    { id: "S044",  tue: "8041 - 8044", cabine1: "8041", cabine2: "8044" },
+    { id: "S048",  tue: "8045 - 8048", cabine1: "8045", cabine2: "8048" },
+
+    { id: "S056",  tue: "8053 - 8056", cabine1: "8053", cabine2: "8056" },
+    { id: "S060",  tue: "8057 - 8060", cabine1: "8057", cabine2: "8060" },
+    { id: "S064",  tue: "8061 - 8064", cabine1: "8061", cabine2: "8064" },
+    { id: "S068",  tue: "8065 - 8068", cabine1: "8065", cabine2: "8068" },
+    { id: "S072",  tue: "8069 - 8072", cabine1: "8069", cabine2: "8072" },
+    { id: "S076",  tue: "8073 - 8076", cabine1: "8073", cabine2: "8076" },
+    { id: "S080",  tue: "8077 - 8080", cabine1: "8077", cabine2: "8080" },
+    { id: "S084",  tue: "8081 - 8084", cabine1: "8081", cabine2: "8084" },
+    { id: "S088",  tue: "8085 - 8088", cabine1: "8085", cabine2: "8088" },
+    { id: "S092",  tue: "8089 - 8092", cabine1: "8089", cabine2: "8092" },
+    { id: "S096",  tue: "8093 - 8096", cabine1: "8093", cabine2: "8096" },
+    { id: "S100",  tue: "8097 - 8100", cabine1: "8097", cabine2: "8100" },
+    { id: "S104",  tue: "8101 - 8104", cabine1: "8101", cabine2: "8104" },
+    { id: "S108",  tue: "8105 - 8108", cabine1: "8105", cabine2: "8108" },
+
+    { id: "S110",  tue: "8051 - 8110", cabine1: "8051", cabine2: "8110" },
+    { id: "S112",  tue: "8049 - 8112", cabine1: "8049", cabine2: "8112" },
+
+    { id: "S116",  tue: "8113 - 8116", cabine1: "8113", cabine2: "8116" },
+    { id: "S120",  tue: "8117 - 8120", cabine1: "8117", cabine2: "8120" },
+    { id: "S124",  tue: "8121 - 8124", cabine1: "8121", cabine2: "8124" },
+    { id: "S128",  tue: "8125 - 8128", cabine1: "8125", cabine2: "8128" },
+    { id: "S132",  tue: "8129 - 8132", cabine1: "8129", cabine2: "8132" },
+    { id: "S136",  tue: "8133 - 8136", cabine1: "8133", cabine2: "8136" },
+    { id: "S140",  tue: "8137 - 8140", cabine1: "8137", cabine2: "8140" },
+    { id: "S144",  tue: "8141 - 8144", cabine1: "8141", cabine2: "8144" }
+
+];
---
+++ linha12_13.js
@@ -0,0 +1,97 @@
+const linha12_13 = [
+
+        // Série 7000
+
+    { id:"Q004", tue:"7001 - 7003", cabine1:"7001", cabine2:"7003" },
+    { id:"Q020", tue:"7019 - 7017", cabine1:"7019", cabine2:"7017" },
+    { id:"Q028", tue:"7027 - 7025", cabine1:"7027", cabine2:"7025" },
+    { id:"Q032", tue:"7029 - 7031", cabine1:"7029", cabine2:"7031" },
+    { id:"Q036", tue:"7033 - 7035", cabine1:"7033", cabine2:"7035" },
+    { id:"Q040", tue:"7039 - 7037", cabine1:"7039", cabine2:"7037" },
+    { id:"Q060", tue:"7057 - 7059", cabine1:"7057", cabine2:"7059" },
+    { id:"Q072", tue:"7069 - 7071", cabine1:"7069", cabine2:"7071" },
+    { id:"Q076", tue:"7075 - 7073", cabine1:"7075", cabine2:"7073" },
+    { id:"Q080", tue:"7077 - 7079", cabine1:"7077", cabine2:"7079" },
+    { id:"Q088", tue:"7085 - 7087", cabine1:"7085", cabine2:"7087" },
+    { id:"Q092", tue:"7091 - 7089", cabine1:"7091", cabine2:"7089" },
+    { id:"Q108", tue:"7105 - 7107", cabine1:"7105", cabine2:"7107" },
+    { id:"Q112", tue:"7109 - 7111", cabine1:"7109", cabine2:"7111" },
+    { id:"Q116", tue:"7115 - 7113", cabine1:"7115", cabine2:"7113" },
+    { id:"Q140", tue:"7139 - 7137", cabine1:"7139", cabine2:"7137" },
+    { id:"Q148", tue:"7145 - 7147", cabine1:"7145", cabine2:"7147" },
+    { id:"Q152", tue:"7151 - 7149", cabine1:"7151", cabine2:"7149" },
+    { id:"Q160", tue:"7159 - 7157", cabine1:"7159", cabine2:"7157" },
+
+    // Série 7500
+
+    { id:"R504", tue:"7503 - 7502", cabine1:"7503", cabine2:"7502" },
+    { id:"R508", tue:"7506 - 7507", cabine1:"7506", cabine2:"7507" },
+    { id:"R512", tue:"7510 - 7511", cabine1:"7510", cabine2:"7511" },
+    { id:"R516", tue:"7516 - 7513", cabine1:"7516", cabine2:"7513" },
+    { id:"R520", tue:"7518 - 7519", cabine1:"7518", cabine2:"7519" },
+    { id:"R524", tue:"7524 - 7521", cabine1:"7524", cabine2:"7521" },
+    { id:"R528", tue:"7525 - 7528", cabine1:"7525", cabine2:"7528" },
+    { id:"R532", tue:"7530 - 7531", cabine1:"7530", cabine2:"7531" },
+
+    // Série 2500
+
+    { id:"U504", tue:"2501 - 2504", cabine1:"2501", cabine2:"2504" },
+    { id:"U508", tue:"2505 - 2508", cabine1:"2505", cabine2:"2508" },
+    { id:"U512", tue:"2509 - 2512", cabine1:"2509", cabine2:"2512" },
+    { id:"U516", tue:"2513 - 2516", cabine1:"2513", cabine2:"2516" },
+    { id:"U520", tue:"2517 - 2520", cabine1:"2517", cabine2:"2520" },
+    { id:"U524", tue:"2521 - 2524", cabine1:"2521", cabine2:"2524" },
+    { id:"U528", tue:"2525 - 2528", cabine1:"2525", cabine2:"2528" },
+    { id:"U532", tue:"2529 - 2532", cabine1:"2529", cabine2:"2532" },
+
+    // Série 8500
+
+    { id:"H504", tue:"8501 - 8504", cabine1:"8501", cabine2:"8504" },
+    { id:"H508", tue:"8505 - 8508", cabine1:"8505", cabine2:"8508" },
+    { id:"H512", tue:"8509 - 8512", cabine1:"8509", cabine2:"8512" },
+    { id:"H516", tue:"8513 - 8516", cabine1:"8513", cabine2:"8516" },
+    { id:"H520", tue:"8517 - 8520", cabine1:"8517", cabine2:"8520" },
+    { id:"H524", tue:"8521 - 8524", cabine1:"8521", cabine2:"8524" },
+    { id:"H528", tue:"8525 - 8528", cabine1:"8525", cabine2:"8528" },
+    { id:"H532", tue:"8529 - 8532", cabine1:"8529", cabine2:"8532" },
+    { id:"H536", tue:"8533 - 8536", cabine1:"8533", cabine2:"8536" },
+    { id:"H540", tue:"8537 - 8540", cabine1:"8537", cabine2:"8540" },
+    { id:"H544", tue:"8541 - 8544", cabine1:"8541", cabine2:"8544" },
+    { id:"H548", tue:"8545 - 8548", cabine1:"8545", cabine2:"8548" },
+    { id:"H552", tue:"8549 - 8552", cabine1:"8549", cabine2:"8552" },
+    { id:"H556", tue:"8553 - 8556", cabine1:"8553", cabine2:"8556" },
+    { id:"H560", tue:"8557 - 8560", cabine1:"8557", cabine2:"8560" },
+    { id:"H564", tue:"8561 - 8564", cabine1:"8561", cabine2:"8564" },
+    { id:"H568", tue:"8565 - 8568", cabine1:"8565", cabine2:"8568" },
+    { id:"H572", tue:"8569 - 8572", cabine1:"8569", cabine2:"8572" },
+    { id:"H576", tue:"8573 - 8576", cabine1:"8573", cabine2:"8576" },
+    { id:"H580", tue:"8577 - 8580", cabine1:"8577", cabine2:"8580" },
+    { id:"H584", tue:"8581 - 8584", cabine1:"8581", cabine2:"8584" },
+    { id:"H588", tue:"8585 - 8588", cabine1:"8585", cabine2:"8588" },
+    { id:"H592", tue:"8589 - 8592", cabine1:"8589", cabine2:"8592" },
+    { id:"H596", tue:"8593 - 8596", cabine1:"8593", cabine2:"8596" },
+    { id:"H600", tue:"8597 - 8600", cabine1:"8597", cabine2:"8600" },
+    { id:"H604", tue:"8601 - 8604", cabine1:"8601", cabine2:"8604" },
+    { id:"H608", tue:"8605 - 8608", cabine1:"8605", cabine2:"8608" },
+    { id:"H612", tue:"8609 - 8612", cabine1:"8609", cabine2:"8612" },
+    { id:"H616", tue:"8613 - 8616", cabine1:"8613", cabine2:"8616" },
+    { id:"H620", tue:"8617 - 8620", cabine1:"8617", cabine2:"8620" },
+    { id:"H624", tue:"8621 - 8624", cabine1:"8621", cabine2:"8624" },
+    { id:"H628", tue:"8625 - 8628", cabine1:"8625", cabine2:"8628" },
+    { id:"H632", tue:"8629 - 8632", cabine1:"8629", cabine2:"8632" },
+    { id:"H636", tue:"8633 - 8636", cabine1:"8633", cabine2:"8636" },
+    { id:"H640", tue:"8637 - 8640", cabine1:"8637", cabine2:"8640" },
+
+    // Série 9000
+
+    { id:"T004", tue:"9001 - 9004", cabine1:"9001", cabine2:"9004" },
+    { id:"T008", tue:"9005 - 9008", cabine1:"9005", cabine2:"9008" },
+    { id:"T012", tue:"9009 - 9012", cabine1:"9009", cabine2:"9012" },
+    { id:"T016", tue:"9013 - 9016", cabine1:"9013", cabine2:"9016" },
+    { id:"T020", tue:"9017 - 9020", cabine1:"9017", cabine2:"9020" },
+    { id:"T024", tue:"9021 - 9024", cabine1:"9021", cabine2:"9024" },
+    { id:"T028", tue:"9025 - 9028", cabine1:"9025", cabine2:"9028" },
+    { id:"T032", tue:"9029 - 9032", cabine1:"9029", cabine2:"9032" },
+    { id:"T036", tue:"9033 - 9036", cabine1:"9033", cabine2:"9036" }
+
+];
